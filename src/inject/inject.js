f174b6c12ee7605f86bb23a616346ca09c0abe60
nihakue <walden.gabrielw@gmail.com>
2014-08-12 16:03:41 -0600

    rolls now updating on click

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,9 +1,12 @@
+rollExps = {
+	'd20': new RegExp("(\\d+ )?(\\w{2,})(?:<\\/b>)? ([+\\-]\\d+)", 'g'),
+	'attack': new RegExp("((\\d+)d(\\d+)\\s?([+\\-]\\d+))", 'g')
+	};
+
 chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
-		var basicRolls = ['Init', 'Fort', 'Ref', 'Will'];
-		var attacks = ['Melee', 'Ranged'];
 
 		// ----------------------------------------------------------
 		// This part of the script triggers when page is done loading
@@ -11,25 +14,31 @@ chrome.extension.sendMessage({}, function(response) {
 		var bodyHTML = document.body.innerHTML;
 
 		//Replace basic stats with roll buttons
-		var re = new RegExp("(\\d+ )?(\\w+)(?:<\\/b>)? ([+\\-]\\d+)", 'g');
-		var match,
-			rollName,
-			modifier,
-			numDice;
+
+		// $('body').contents().filter(function() {
+		// 	return this.nodeType === 3;
+		// }).each(function() {
+		// 	$(this).replaceWith($(this).text().replace(re, d20replacer));
+		// });
 
 		//We might find a better way to do this, but the lack of structure forces us to rely on the text itself.
-		bodyHTML = bodyHTML.replace(re, d20replacer);	
+		bodyHTML = bodyHTML.replace(rollExps.d20, d20replacer);	
 
 		//replace damage rolls with roll buttons
-		var re = new RegExp("((\\d+)d(\\d+)\\s?([+\\-]\\d+))", 'g');
-		var matches = re.exec(bodyHTML);
-		bodyHTML = bodyHTML.replace(re, replacer);
+		bodyHTML = bodyHTML.replace(rollExps.attack, replacer);
 
 		document.body.innerHTML = bodyHTML;
 
 		//Initialize Popover for every popover element
 		$('[data-toggle="popover"').popover();
 		// $('.roll').on('click', function(e){$(this).attr("data-content", roll(1, 20, $(this).attr("modifier")))});
+
+		$('.roll').on('click', function(e) {
+			var numDice = $(this).attr('numDice');
+			var die = $(this).attr('die');
+			var modifier = $(this).attr('modifier');
+			$(this).attr('data-content', roll(numDice, die, modifier));
+		});
 		// ----------------------------------------------------------
 
 	}
@@ -37,25 +46,34 @@ chrome.extension.sendMessage({}, function(response) {
 });
 
 function d20replacer(match, p1, p2, p3){
-	return createRollButton({'rollName': (p1 || "") + " " + p2 + p3, 'modifier': p3, 'die': 20, 'numDice': p1 || 1})[0].outerHTML;
+	return createRollButton({'rollName': (p1 || "") + " " + p2 + " " + p3, 'modifier': p3, 'die': 20, 'numDice': p1 || 1})[0].outerHTML;
 }
 
 function replacer(match, p1, p2, p3, p4){
-	return createRollButton({'rollName': match, 'modifier': p4, 'die': p3, 'numDice': p2 || 1})[0].outerHTML;
+	return createRollButton({'buttonType': 'btn-danger', 'rollName': match, 'modifier': p4, 'die': p3, 'numDice': p2 || 1})[0].outerHTML;
 }
 
-function rollString(s){
-	var re = new RegExp("(\\d+)d(\\d+)\\s?([+\\-]\\d+)");
-	var matches = re.exec(s);
-	if (matches.length == 4){
-		var numDice = matches[1];
-		var die = matches[2];
-		var modifier = matches[3];
-	} else{
-		return null;
-	}
-	return roll(numDice, die, modifier);
-}
+//broken at present
+// function rollString(s){
+// 	var matches,
+// 		numDice,
+// 		die,
+// 		modifier;
+
+// 	if (matches = rollExps.attack.exec(s)){
+// 		numDice = matches[1];
+// 		die = matches[2];
+// 		modifier = matches[3];
+// 	} else if (matches = rollExps.d20.exec(s)){
+// 		numDice = matches[1];
+// 		die = 20;
+// 		modifier = matches[3];
+// 	} else{
+// 		console.log("rollString broke");
+// 		return null;
+// 	}
+// 	return roll(numDice, die, modifier);
+// }
 
 function roll(numDice, die, modifier){
 	modifierInt = parseInt(modifier);
@@ -66,40 +84,37 @@ function roll(numDice, die, modifier){
 }
 
 function rollFactory(numDice, die, modifier){
-	var numDice = numDice;
-	var die = die;
-	var modifier = modifier;
-	function newRoll(){
+	// console.log('Die outer: ' + die);
+	return function (){
+		// console.log('Die inner: ' + die);
 		return roll(numDice, die, modifier);
 	}
-	return newRoll;
 }
 
 function createRollButton(options){
-	console.log(options);
 	var rollName = typeof(options.rollName) === "undefined" ? "Basic" : options.rollName;
 	var modifier = typeof(options.modifier) === "undefined" ? "+0" : options.modifier;
 	var die = typeof(options.die) === "undefined" ? 20 : options.die;
 	var numDice = typeof(options.numDice) === "undefined" ? 1 : options.numDice;
+	var buttonType = typeof(options.buttonType) === "undefined" ? "btn-success" : options.buttonType;
 
-	var rollClick = function(e){
-		$(this).attr("data-content", roll(numDice, die, modifier));
-	}
+	
 
 	var rollButton = $('<button></button>', {
 		'type': 'button',
-		'id': rollName,
-		'class': "btn btn-success btn-xs popover-dismiss roll",
+		'class': "btn " + buttonType + " btn-xs popover-dismiss roll",
 		'data-trigger': "click",
 		'data-toggle': "popover",
 		'title': rollName + " roll",
 		'text': rollName,
-		'data-content': roll(numDice, die, modifier)})
-		.on('click', function(e) {
-			$(this).attr("data-content", rollFactory(numDice, die, modifier));
+		'data-content': roll(numDice, die, modifier),
+		'rollName': rollName,
+		'modifier': modifier,
+		'die': die,
+		'numDice': numDice
 		});
+// .on('click', function(e) {
+// 			$(this).attr("data-content", rollFactory(numDice, die, modifier));
 
-	
 	return rollButton;
-	// return '<button onclick=rollClick type="button" id="rollName" modifier="' + modifier + '" class="btn btn-success btn-xs popover-dismiss roll" data-trigger="click" data-toggle="popover" title="' + rollName + ' roll" data-content="' + roll(numDice, die, modifier) + '">' + rollName + '</button>';
 }
