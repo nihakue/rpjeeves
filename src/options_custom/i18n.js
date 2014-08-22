cb34efef2b48f4bfe18d5f41a8afe6ed115c7153
nihakue <walden.gabrielw@gmail.com>
2014-08-22 12:37:18 -0600

    Options page prepped for alpha release

--- src/options_custom/i18n.js
+++ src/options_custom/i18n.js
@@ -12,60 +12,20 @@ this.i18n = {
         "en": "No matches were found.",
         "de": "Keine Übereinstimmungen gefunden."
     },
-    
-    
-    
     "information": {
         "en": "Information",
         "de": "Information"
     },
-    "login": {
-        "en": "Login",
-        "de": "Anmeldung"
-    },
-    "username": {
-        "en": "Username:",
-        "de": "Benutzername:"
-    },
-    "password": {
-        "en": "Password:",
-        "de": "Passwort:"
-    },
-    "x-characters": {
-        "en": "6 - 12 characters",
-        "de": "6 - 12 Zeichen"
-    },
-    "x-characters-pw": {
-        "en": "10 - 18 characters",
-        "de": "10 - 18 Zeichen"
+    "welcome": {
+        "en": "Welcome",
+        "de": "Vilkommen"
     },
     "description": {
-        "en": "This is a description. You can write any text inside of this.<br>\
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut\
-        labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores\
-        et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem\
-        ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et\
-        dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.\
-        Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
-        
-        "de": "Das ist eine Beschreibung. Du kannst hier beliebigen Text einfügen.<br>\
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut\
-        labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores\
-        et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem\
-        ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et\
-        dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.\
-        Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
-    },
-    "logout": {
-        "en": "Logout",
-        "de": "Abmeldung"
+        "en": "rpjeeves is in closed beta right now, and we're working on adding new features all the time. If you encounter a bug, please report it on <a href='https://github.com/nihakue/rpjeeves'>our github page</a>. Thanks! I hope you enjoy using rpjeeves.",
+        "de": "rpjeeves is in closed beta right now, and we're working on adding new features all the time. If you encounter a bug, please report it on <a href='https://github.com/nihakue/rpjeeves'>our github page</a>. Thanks! I hope you enjoy using rpjeeves."
     },
     "enable": {
         "en": "Enable",
         "de": "Aktivieren"
-    },
-    "disconnect": {
-        "en": "Disconnect:",
-        "de": "Trennen:"
     }
 };
